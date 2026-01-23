294a3128226391c9985f06ca4c1b597d0c8076d7
kadalsaune <kadalsaune@gmail.com>
2026-01-23 13:21:27 +0100

    initital commit

---
+++ react-facts/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './assets/components/Header.jsx'
+import MainContent from './assets/components/MainContent.jsx'
+
+function App() {
+
+  return (
+    <>
+     <Header />
+     <MainContent />
+    </>
+  )
+}
+
+export default App
---
+++ react-state/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeIcon from '../assets/chef-claude-icon.png';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src={chefClaudeIcon} alt="Chef Claude Icon" /><h1>  Chef Claude</h1>
+    </header>
+  );
+}
+
